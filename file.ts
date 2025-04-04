ad1284c5fe32a86bcec7926e89ea0baf7166c9df
August Saint Freytag <me@augustfreytag.com>
2025-04-04 16:34:28 +0200

    Rewrite instance property internals
    Use local variable to store value.
    Support default property values on init.

--- file.ts
+++ file.ts
@@ -1,25 +1,55 @@
 export function InstanceProperty() {
-	return function (_value: unknown, context: ClassFieldDecoratorContext) {
+	return function (value: any, context: ClassFieldDecoratorContext) {
 		const { name } = context
-		const dataPropertyName = `__${String(name)}`
 
 		return function (this: any, initialValue: unknown) {
-			this[dataPropertyName] = initialValue
+			let didInitialize = false
+			let storedValue = initialValue
 
 			Object.defineProperty(this, name, {
 				get() {
-					console.log(`Reading and returning value for property ${String(name)}, ${this[dataPropertyName]}`)
-					return this[dataPropertyName]
+					console.log(
+						`Reading and returning value for property ${String(
+							name
+						)}, ${storedValue}`
+					)
+
+					return storedValue
 				},
 				set(newValue) {
-					console.log(`Setting value for property ${String(name)} to ${newValue}`)
-					this[dataPropertyName] = newValue
+					if (!didInitialize) {
+						didInitialize = true
+
+						if (
+							newValue === undefined &&
+							initialValue !== undefined
+						) {
+							console.log(
+								`Setting value for property ${String(
+									name
+								)} to initial value ${initialValue}`
+							)
+
+							storedValue = initialValue
+							return
+						}
+					}
+
+					console.log(
+						`Setting value for property ${String(
+							name
+						)} to ${newValue}`
+					)
+
+					storedValue = newValue
 				},
 				enumerable: true,
 				configurable: true
 			})
 
-			return undefined
+			console.log(context.metadata)
+
+			return value
 		}
 	}
 }
@@ -29,12 +59,21 @@ class Item {
 	@InstanceProperty()
 	public value: number = 5
 
-	constructor(value: number) {
-		this.value = value
+	constructor(value?: number) {
+		// this.value = value
 	}
 }
 
-const instance = new Item(20)
-instance.value = 10
+const instanceA = new Item()
+const instanceB = new Item()
 
-console.log(Object.getOwnPropertyDescriptor(instance, "value"))
+instanceA.value
+instanceA.value = 10
+
+instanceB.value
+instanceB.value = 25
+
+console.log(instanceA.value)
+console.log(instanceB.value)
+
+// console.log(Object.getOwnPropertyDescriptor(instance, "value"))
